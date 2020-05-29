0f276dffdd7c1ab63ab945c2d299877cea436c23
Ky Phokkinn <phokkinn.ky@gmail.com>
2020-05-29 09:54:01 +0700

    Update server.js
    update database connect

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ require('./app/controllers/socketIO')(io);
 mongoose.Promise = global.Promise;
 global.appRoot = path.resolve(__dirname);
 
-mongoose.connect('mongodb://localhost:27017/livestream', (err) => {
+mongoose.connect('mongodb://upetjhl1lomanzvjzhwj:G4nSsXr9VFmXnd6yXSpo@b645tlpaeflftbj-mongodb.services.clever-cloud.com:27017/b645tlpaeflftbj', (err) => {
   if (err) {
     console.log('....................... ERROR CONNECT TO DATABASE');
     console.log(err);
